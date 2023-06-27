849ee823aba9192af1a060e62609b90df70d57f3
Thiendn04 <thiendn04@gmail.com>
2023-06-27 11:00:21 +0700

    sesion

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 function Home() {
+    const [user, setName] = useState('')
+    const navigate = useNavigate()
+
+    axios.defaults.withCredentials = true;
+    useEffect(() => {
+        axios.get('http://localhost:8081')
+        .then( res => {
+            if(res.data.valid) {
+                setName(res.data.name);
+            } else {
+                navigate('/login')
+            }
+        })
+        .catch(err => console.log(err))
+    }, [])
     return (
-        <div>Home</div>  
+        <div><h1>Wellcome {user}</h1></div>  
     )
 }
 export default Home
--- server/server.js
+++ server/server.js
@@ -1,18 +1,44 @@
-const express = require("express");
-const mysql = require('mysql');
-const cors = require('cors');
-const { check, validationResult } = require('express-validator');
-
+import express from 'express'
+import mysql from 'mysql';
+import cors from 'cors';
+// import { check, validationResult, cookie } from 'express-validator';
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import bodyParser from 'body-parser';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 24
+    }
+}))
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "123456",
     database: "signup"
 })
+
+app.get('/', (req, res) => {
+    if(req.session.name) {
+        return res.json({valid: true, name: req.session.name})
+    } else {
+        return res.json({valid: false})
+    }
+})
+
 app.post('/signup', (req, res) => {
     const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
     const values = [
@@ -27,23 +53,17 @@ app.post('/signup', (req, res) => {
         return res.json(data);
     })
 })
-app.post('/login',[check('email', "Emaill length error").isEmail().isLength({min: 10, max:30}),check('password', "password length 8-10").isLength({min: 8, max: 10})], (req, res) => {
+app.post('/login', (req, res) => {
     const sql = "SELECT * FROM login WHERE email = ? AND password = ?";
     db.query(sql, [req.body.email,req.body.password ], (err, data) => {
-        const errors = validationResult(req);
-            if(!errors.isEmpty()) {
-                return res.json(errors);
-                } else {
-                    if(err) {
-                        return res.json("Error");
-                    }
-                    if(data.length > 0) {
-                        return res.json("Success");
-                    } else {
-                        return res.json("Faile");
-                    }
-                }
-        })
+        if(err) return res.json({Message: "Error inside server"});
+        if(data.length > 0) {
+            req.session.name = data[0].name;
+            return res. json({Login: true})
+        } else {
+            return res.json({Login: false})
+        }
+    })
 })
 app.listen(8081, ()=> {
     console.log("listening");
